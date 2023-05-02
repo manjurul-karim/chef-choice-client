f2b840363a640e30832ce24d2408f88366b29210
manjurul17002 <manjurul17002@gmail.com>
2023-05-03 03:31:28 +0600

    added banner,extra section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div className='flex  justify-between px-8 pt-4'>
+        <div className='flex  justify-between px-8 py-4 bg-red-500'>
             <h4 className='tex-2xl font-bold uppercase'> <span className='font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>chef's Choice</span></h4>
             <div className='mr-2'>
             <Link className='mr-2' to='/'>Home</Link>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable no-unused-vars */
+import React from "react";
+import banner from "../assets/banner.png";
+import party1 from "../assets/party-1.jpg";
+import party2 from "../assets/party-2.jpg";
+import party3 from "../assets/party-3.jpg";
+import may17 from "../assets/may-17.jpg";
 
 const Home = () => {
-    return (
-        <div>
-           <div>
-           <div className="hero min-h-screen" style={{ backgroundImage: `url("/images/stock/photo-1507358522600-9f71e620c44e.jpg")` }}>
+  return (
+    <div className="bg-blue-500">
+      <div>
+        <div className="hero min-h-screen ">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <img src={banner} className=" w-2/3 rounded-md " />
+            <div className="pl-12">
+              <h1 className="text-5xl  font-bold">
+                Welcome To the Chef's Choice
+              </h1>
+              <p className="py-6">
+                Amazing Food for Amazing People.Lot of bengali dishes are
+                available made by world's famous chef.Our Comitment is Making
+                hygenic and Healthy food .
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* <div className="hero min-h-screen" style={{ backgroundImage: `url("../assets/banner.jpg")` }}>
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-md">
@@ -13,10 +36,207 @@ const Home = () => {
       <button className="btn btn-primary">Get Started</button>
     </div>
   </div>
-</div>
-           </div>
+</div> */}
+      </div>
+      <div>
+        {/* ! Chef Card Section Start */}
+
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              Shoes!
+              <div className="badge badge-secondary">NEW</div>
+            </h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-start">
+              <div className="badge badge-outline">Fashion</div>
+              <div className="badge badge-outline">Products</div>
+            </div>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+      {/* chef Card Section End */}
+
+      {/* Extra Section One Start */}
+
+      <div className="my-28">
+        <h2 className="text-center font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+          {" "}
+          What Our Customer's Have to Say
+        </h2>
+        <div className="flex flex-col md:flex-row justify-between gap-5 mx-4  ">
+          <div className="">
+            <h2>Beautiful View And Healthy Food</h2>
+
+            <div className="rating">
+              <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-orange-400"
+              />
+              <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-orange-400"
+              />
+              <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-orange-400"
+              />
+              <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-orange-400"
+              />
+              <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-orange-400"
+                checked
+              />
+            </div>
+            <div>
+              <p>
+                {" "}
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
+                earum totam similique unde itaque ducimus mollitia tempore rerum
+                eaque adipisci dolorum voluptates, quisquam aliquam neque
+                exercitationem odit iusto iure numquam cumque corporis.
+                Veritatis debitis rem, modi cum quis numquam provident harum
+                accusantium delectus similique eos facilis quisquam, corporis
+                voluptatum? Temporibus.
+              </p>
+            </div>
+          </div>
+          <div>
+            <div className="">
+              <h2>Beautiful View And Healthy Food</h2>
+
+              <div className="rating">
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                  checked
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+              </div>
+              <div>
+                <p>
+                  {" "}
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
+                  earum totam similique unde itaque ducimus mollitia tempore
+                  rerum eaque adipisci dolorum voluptates, quisquam aliquam
+                  neque exercitationem odit iusto iure numquam cumque corporis.
+                  Veritatis debitis rem, modi cum quis numquam provident harum
+                  accusantium delectus similique eos facilis quisquam, corporis
+                  voluptatum? Temporibus.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Extra Section One End */}
+
+      {/* Extra Section Two Start */}
+
+      <div className=" ml-5 mr-5 ">
+        <h2 className=" my-12 text-center font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+          UPComing Calander Of Event
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 pb-8 ">
+          <div className=" bg-base-100 rounded mb-2 p-4 ">
+            <div className="flex gap-4 mb-4">
+              <img className="h-12 w-12 rounded" src={may17} alt="" />
+              <div>
+                <h2 className="card-title">New album is released!</h2>
+                <p>May 09,2023 - 8.00 PM</p>
+              </div>
+            </div>
+            <div className="flex">
+              <figure>
+                <img className="h-36 w-72 rounded" src={party1} alt="Album" />
+              </figure>
+              <div className="card-body">
+                <p>Click the button to listen on Spotiwhy app.</p>
+              </div>
+            </div>
+          </div>
+          <div className=" bg-base-100 rounded mb-2  p-4 ">
+            <div className="flex gap-4 mb-4 ">
+              <img className="h-12 w-12 rounded" src={may17} alt="" />
+              <div>
+                <h2 className="card-title"></h2>
+                <p>May 17,2023 - 10.00 PM</p>
+              </div>
+            </div>
+            <div className="flex">
+              <figure>
+                <img
+                  className="h-36 w-72 rounded-sm"
+                  src={party2}
+                  alt="Album"
+                />
+              </figure>
+              <div className="card-body">
+                <p>Click the button to listen on Spotiwhy app.</p>
+              </div>
+            </div>
+          </div>
+          <div className=" bg-base-100 rounded mb-2 p-4  ">
+            <div className="flex gap-4 mb-4">
+              <img className="h-12 w-12 rounded" src={may17} alt="" />
+              <div>
+                <h2 className="card-title">Girl's hangOut !</h2>
+                <p>May 21,2023 - 6.00 PM</p>
+              </div>
+            </div>
+            <div className="flex">
+              <figure>
+                <img className="h-36 w-72" src={party3} alt="Album" />
+              </figure>
+              <div className="card-body">
+                <p>Click the button to listen on Spotiwhy app.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    //  ! Extra Section two End
+  );
 };
 
 export default Home;
