a7c54d725ae2332e6426118d5292eb117a8af664
manjurul17002 <manjurul17002@gmail.com>
2023-05-03 00:33:23 +0600

    intial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className='flex  justify-between px-8 pt-4'>
+            <h4 className='tex-2xl font-bold uppercase'> <span className='font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>chef's Choice</span></h4>
+            <div className='mr-2'>
+            <Link className='mr-2' to='/'>Home</Link>
+            <Link className='mr-2' to='/blog'>Blog</Link>
+            <Link className='mr-2' to='/'>Profilepicture</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCfAXLNEsNCoEQgsypkMVmktKLQXj7gVtU",
+  authDomain: "assignment-10-e978e.firebaseapp.com",
+  projectId: "assignment-10-e978e",
+  storageBucket: "assignment-10-e978e.appspot.com",
+  messagingSenderId: "742506543889",
+  appId: "1:742506543889:web:af348a7e1e34c3b7ed2ab0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
