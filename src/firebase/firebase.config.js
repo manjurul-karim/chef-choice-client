f0dfd132858a791db69fc3b97b35217f001cc33e
manjurul17002 <manjurul17002@gmail.com>
2023-05-04 19:00:51 +0600

    firebase hide confiq

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -7,12 +7,12 @@ import { initializeApp } from "firebase/app";
 
 console.log('environment variable' , import.meta.env.VITE_PASS);
 const firebaseConfig = {
-  apiKey: "AIzaSyCfAXLNEsNCoEQgsypkMVmktKLQXj7gVtU",
-  authDomain: "assignment-10-e978e.firebaseapp.com",
-  projectId: "assignment-10-e978e",
-  storageBucket: "assignment-10-e978e.appspot.com",
-  messagingSenderId: "742506543889",
-  appId: "1:742506543889:web:af348a7e1e34c3b7ed2ab0"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
